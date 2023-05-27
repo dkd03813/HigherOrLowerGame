49f7d84b2e2dae8a15c5efc12e9032ae175e0339
Erica Bitting <ericabitting@gmail.com>
2023-05-27 12:15:17 -0400

    added elements to the game page using hard code data, and added a nav bar to both game page and home page

---
+++ data.js
@@ -0,0 +1,23 @@
+// let hardCodeMusicData = [
+// 	{
+// 		Album: "1 (Beatles Album)",
+// 		Year: "2000",
+// 		Type: "album",
+// 		Cover:
+// 			"https://upload.wikimedia.org/wikipedia/en/b/be/The_Beatles_1_album_cover.jpg"
+// 	},
+// 	{
+// 		Title: "The Very Best of the Beach Boys",
+// 		Year: "2001",
+// 		Type: "album",
+// 		Cover:
+// 			"https://upload.wikimedia.org/wikipedia/en/7/73/Very_Best_of_The_Beach_Boys_cover.jpg"
+// 	},
+// ];
+
+// console.log(hardCodeMusicData);
+// // hardCodeMusicData.forEach((hardCodeMusicData) => console.log(hardCodeMusicData));
+
+// const musicDIV = document.querySelector(".results");
+
+// console.log(musicDIV);
